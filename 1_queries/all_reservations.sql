a723ed39f97578cf7876aba1b4d43e315b6becdc
Lisa Cormier <lcormier5@hotmail.com>
2021-09-15 22:03:59 +0000

    adding all SELECT queries

---
+++ 1_queries/all_reservations.sql
@@ -0,0 +1,9 @@
+SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE end_date < now()::date
+AND reservations.guest_id = 1
+GROUP BY reservations.id, properties.id
+ORDER BY start_date
+LIMIT 10;
